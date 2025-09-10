ec81485317764695a9ab1638741fdf9612e8a249
faleite <fabricio_2310@hotmail.com>
2025-09-10 22:51:45 +0100

    (Add) Implement base model classes and configure database connection

---
+++ picpay/src/main/java/com/faleite/picpay/model/Merchant.java
@@ -0,0 +1,28 @@
+package com.faleite.picpay.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_mercahnts")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Merchant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String cnpj;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
